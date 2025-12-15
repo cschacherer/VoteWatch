374e3a6c8b3ec9c66566866fe3f8352094caf7a7
cschacherer <connerschacherer@gmail.com>
2025-12-15 13:51:50 -0700

    get rid of nodemodules in repository

--- server/billRouter.js
+++ server/billRouter.js
@@ -1,48 +1,51 @@
-import express from 'express'
-import Database from './database/database.js'
+import express from "express";
+import Database from "./database/database.js";
 
 const billRouter = express.Router();
 
-const _db = new Database();
-await _db.openDatabase();
-
-billRouter.get('/', async (req, res) => {
+try {
+    const _db = new Database();
+    await _db.openDatabase();
+} catch (err) {
+    console.error("Error opening database:", err);
+}
+billRouter.get("/", async (req, res) => {
     try {
-        console.log('get all bills');
+        console.log("get all bills");
         //send back the bill id information
         const allBills = await _db.getAllBills();
         res.send(allBills);
     } catch (err) {
-        console.error('Error fetching bill details:', error);
-        res.status(500).send('Internal Server Error');
+        console.error("Error fetching bill details:", error);
+        res.status(500).send("Internal Server Error");
     }
 });
 
 //next is when you need to handoff to another function.  You don't need it for everything
-billRouter.get('/:id', async (req, res) => {
+billRouter.get("/:id", async (req, res) => {
     try {
         //send back the bill id information
-        console.log('get bill with id');
+        console.log("get bill with id");
         const id = req.params.id;
         console.log(id);
         const billDetails = await _db.getBill(id);
         res.send(billDetails);
     } catch (err) {
-        console.error('Error fetching bill details:', error);
-        res.status(500).send('Internal Server Error');
+        console.error("Error fetching bill details:", error);
+        res.status(500).send("Internal Server Error");
     }
 });
 
-billRouter.get('/:id/votes', async (req, res) => {
+billRouter.get("/:id/votes", async (req, res) => {
     try {
         //send back the bill id information
         const id = req.params.id;
         const allVotes = await _db.getAllVotesOnBill(id);
         res.send(allVotes);
     } catch (err) {
-        console.error('Error fetching bill details:', error);
-        res.status(500).send('Internal Server Error');
+        console.error("Error fetching bill details:", error);
+        res.status(500).send("Internal Server Error");
     }
 });
 
-export { billRouter }; 
+export { billRouter };
--- server/legislatorRouter.js
+++ server/legislatorRouter.js
@@ -1,49 +1,54 @@
-import express from 'express';
-import Database from './database/database.js'
+import express from "express";
+import Database from "./database/database.js";
 
 const legislatorRouter = express.Router();
 
-const _db = new Database();
-await _db.openDatabase();
+try {
+    const _db = new Database();
+    await _db.openDatabase();
+} catch (err) {
+    console.error("Error opening database:", err);
+}
 
-
-legislatorRouter.get('/', async (req, res) => {
-    //send back the legislator id information        console.log('get all bills'); 
+legislatorRouter.get("/", async (req, res) => {
+    //send back the legislator id information        console.log('get all bills');
     try {
-        console.log('get all legislators');
+        console.log("get all legislators");
 
         const allLegislators = await _db.getAllLegislators();
         res.send(allLegislators);
     } catch (err) {
-        console.error('Error fetching legislator details:', err);
-        res.status(500).send('Internal Server Error');
+        console.error("Error fetching legislator details:", err);
+        res.status(500).send("Internal Server Error");
     }
 });
 
-legislatorRouter.get('/:id', async (req, res) => {
+legislatorRouter.get("/:id", async (req, res) => {
     //send back the legislator id information
     try {
-        console.log('get legislator with id');
+        console.log("get legislator with id");
 
         const id = req.params.id;
         const legislatorData = await _db.getLegislator(id);
         res.send(legislatorData);
     } catch (err) {
-        console.error('Error fetching legislator details:', err);
-        res.status(500).send('Internal Server Error');
+        console.error("Error fetching legislator details:", err);
+        res.status(500).send("Internal Server Error");
     }
 });
 
-legislatorRouter.get('/:id/votes', async (req, res) => {
+legislatorRouter.get("/:id/votes", async (req, res) => {
     try {
-        console.log('get all votes for legislator');
+        console.log("get all votes for legislator");
         const id = req.params.id;
-        const allLegislatorVotes = await _db.getAllBillsAndVotesForLegislator(id)
+        const allLegislatorVotes = await _db.getAllBillsAndVotesForLegislator(
+            id
+        );
         res.send(allLegislatorVotes);
     } catch (err) {
-        console.error('Error fetching votes for legislator:', err);
-        res.status(500).send('Internal Server Error');
+        console.error("Error fetching votes for legislator:", err);
+        res.status(500).send("Internal Server Error");
     }
-})
+});
 
-export { legislatorRouter }; 
+export { legislatorRouter };
